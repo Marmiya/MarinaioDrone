fdbcff99670141c04e1707ff217396201b0d3b4d
Mingyang Wang <michael.gonw@gmail.com>
2022-03-27 19:21:08 +0800

    ibs finished.

--- include/ibs.cpp
+++ include/ibs.cpp
@@ -26,25 +26,190 @@ generateMidpts(const SurfaceMesh& mesh)
 	return midpts;
 }
 
+void
+draw_polygon(
+	FILE* fp, std::vector<int>& f_vert, std::vector<double>& v, int j)
+{
+	static char s[6][128];
+	int k, l, n = f_vert[j];
+
+	// Create POV-Ray vector strings for each of the vertices
+	for (k = 0; k < n; k++) {
+		l = 3 * f_vert[j + k + 1];
+		sprintf(s[k], "<%g,%g,%g>", v[l], v[l + 1], v[l + 2]);
+
+	}
+
+	// Draw the interior of the polygon
+	fputs("union{\n", fp);
+	for (k = 2; k < n; k++) fprintf(fp, "\ttriangle{%s,%s,%s}\n", s[0], s[k - 1], s[k]);
+	fputs("\ttexture{t1}\n}\n", fp);
+
+	// Draw the outline of the polygon
+	fputs("union{\n", fp);
+	for (k = 0; k < n; k++) {
+		l = (k + 1) % n;
+		fprintf(fp, "\tcylinder{%s,%s,r}\n\tsphere{%s,r}\n",
+			s[k], s[l], s[l]);
+
+	}
+	fputs("\ttexture{t2}\n}\n", fp);
+
+}
+
+inline bool
+ifneighbor(
+	std::vector<size_t> ptsSize,
+	const size_t& a, const size_t& b)
+{
+	size_t aclass = ptsSize.size();
+	size_t total = 0;
+	for (size_t i = 0; i < ptsSize.size(); i++)
+	{
+		total += ptsSize.at(i);
+		if (total > a)
+		{
+			aclass = i;
+			break;
+		}
+	}
+	if (aclass == ptsSize.size())
+	{
+		throw;
+	}
+	size_t totalbb = std::reduce(ptsSize.begin(), ptsSize.begin() + aclass);
+	size_t totalba = totalbb + ptsSize.at(aclass);
+	if (b >= totalbb && b < totalba)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 SurfaceMesh
 IBSCreating(
-	const SurfaceMesh& objl, const SurfaceMesh& objr, const double& bbExpandBias,
-	const double& expectedArea
+	const std::vector<SurfaceMesh>& objs,
+	const double& bbExpandBias, const double& expectedArea
 )
 {
-	SurfaceMesh lmesh = cgaltools::averaged(objl, expectedArea), rmesh = cgaltools::averaged(objr, expectedArea);
-	SurfaceMesh ori(objl);
-	ori += objr;
+	SurfaceMesh ori;
+	SurfaceMesh ans;
+
+	size_t objsSize = objs.size();
+	
+	std::vector<SurfaceMesh> meshs(objsSize);
+
+	for (size_t i = 0; i < objsSize; i++)
+	{
+		meshs.at(i) = cgaltools::averaged(objs.at(i), expectedArea);
+		ori += objs.at(i);
+	}
+
 	SurfaceMesh obb = cgaltools::obb(ori, bbExpandBias);
 
-	Tree ltree(lmesh.faces().begin(), lmesh.faces().end(), lmesh);
-	Tree rtree(rmesh.faces().begin(), rmesh.faces().end(), rmesh);
+	std::vector<PointSet3> ptss(objsSize, true);
 
-	PointSet3 lmidpts(true), rmidpts(true);
-	lmidpts = generateMidpts(lmesh);
-	rmidpts = generateMidpts(rmesh);
+	for (size_t i = 0; i < objsSize; i++)
+	{
+		ptss.at(i) = generateMidpts(meshs.at(i));
+	}
 
-	voro::container con(0, 1, 0, 1, 0, 1, 5, 5, 5, false, false, false, 8);
+	std::vector<size_t> midptsSize(objsSize);
+	for (size_t i = 0; i < objsSize; i++)
+	{
+		midptsSize.at(i) = ptss.at(i).size();
+	}
 
-	return obb;
+	double ax = 363363., ay = ax, az = ay;
+	double bx = -363363., by = bx, bz = by;
+	for (const auto& i : obb.points())
+	{
+		if (i.x() < ax)
+		{
+			ax = i.x();
+		}
+		if (i.x() > bx)
+		{
+			bx = i.x();
+		}
+		if (i.y() < ay)
+		{
+			ay = i.y();
+		}
+		if (i.y() > by)
+		{
+			by = i.y();
+		}
+		if (i.z() < az)
+		{
+			az = i.z();
+		}
+		if (i.z() > bz)
+		{
+			bz = i.z();
+		}
+	}
+
+	voro::container con(ax, bx, ay, by, az, bz, 50, 50, 50, false, false, false, 8);
+
+	size_t startIndex = 0;
+	for (size_t i = 0; i < objsSize; i++)
+	{
+		for (size_t j = 0; j < midptsSize.at(i); j++)
+		{
+			con.put(j + startIndex, ptss.at(i).point(j).x(), ptss.at(i).point(j).y(), ptss.at(i).point(j).z());
+		}
+		startIndex += midptsSize.at(i);
+	}
+
+	double x, y, z;
+	int id;
+	voro::voronoicell_neighbor c;
+	std::vector<int> neigh, f_vert;
+	std::vector<double> v;
+	
+	voro::c_loop_all cl(con);
+
+	size_t i, j;
+	if (cl.start()) 
+		do if (con.compute_cell(c, cl)) {
+			cl.pos(x, y, z); id = cl.pid();
+
+			// Gather information about the computed Voronoi cell
+			c.neighbors(neigh);
+			c.face_vertices(f_vert);
+			c.vertices(x, y, z, v);
+			
+			// Loop over all faces of the Voronoi cell
+			for (i = 0, j = 0; i < neigh.size(); i++) 
+			{
+				if (neigh[i] > id && !ifneighbor(midptsSize, id, neigh[i]))
+					
+				{
+
+					int k, l, n = f_vert[j];
+					std::vector<SMVI> ptsVI;
+
+					for (k = 0; k < n; k++) {
+						l = 3 * f_vert[j + k + 1];
+						ptsVI.push_back(ans.add_vertex(Point3(v[l], v[l + 1], v[l + 2])));
+					}
+
+					ans.add_face(ptsVI);
+
+				}
+				
+				// Skip to the next entry in the face vertex list
+				j += f_vert[j] + 1;
+
+			}
+
+		} while (cl.inc());
+
+	//con.draw_particles("C:\\Users\\mic\\Documents\\gnuplot\\random_points_p.gnu");
+	//con.draw_cells_gnuplot("C:\\Users\\mic\\Documents\\gnuplot\\random_points_v.gnu");
+	return ans;
 }
--- include/ibs.h
+++ include/ibs.h
@@ -4,8 +4,17 @@
 PointSet3
 generateMidpts(const SurfaceMesh& mesh);
 
+void
+draw_polygon(
+	FILE* fp, std::vector<int>& f_vert, std::vector<double>& v, int j);
+
+inline bool
+ifneighbor(
+	std::vector<size_t> ptsSize,
+	const size_t& a, const size_t& b);
+
 SurfaceMesh
 IBSCreating(
-	const SurfaceMesh& objl, const SurfaceMesh& objr, const double& bbExpandBias,
-	const double& expectedArea
+	const std::vector<SurfaceMesh>& objs, 
+	const double& bbExpandBias,	const double& expectedArea
 );
